0901f4e21a5c46f31a23c1ae6c648dd7d726fca9
kwayne <wkudoi@gmail.com>
2019-10-24 02:49:01 +0300

    creation of article class to define arcticle obj

--- app/models.py
+++ app/models.py
@@ -11,3 +11,17 @@ class Source:
         self.category = category
         self.language = language
         self.country = country
+        
+
+class Article:
+    '''
+    Article class to define article objects
+    '''
+
+    def __init__(self, author, title, description, url, urlToImage, publishedAt):
+        self.author = author
+        self.title = title
+        self.description = description
+        self.url = url
+        self.urlToImage = urlToImage
+        self.publishedAt = publishedAt
