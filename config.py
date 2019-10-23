f54fa246caaa9ec1dd2d9bf0e727d254389208fe
kwayne <wkudoi@gmail.com>
2019-10-24 02:43:02 +0300

    setup production child class

--- config.py
+++ config.py
@@ -9,3 +9,13 @@ class Config:
     NEWS_API_BASE_URL = 'https://newsapi.org/v2/sources?language=en&category={}&apiKey={}'
     NEWS_ARTICLE_URL = 'https://newsapi.org/v2/everything?language=en&sources={}&apiKey={}'
     NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+    
+
+class ProdConfig(Config):
+    '''
+    Production configuration child class
+
+    Args:
+        Config: The parent configuration class with general configuration settings
+    '''
+    pass
