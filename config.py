05558256ec0f1c31ec8c5f4c20c0682a458ef891
kwayne <wkudoi@gmail.com>
2019-10-24 02:44:11 +0300

    setup development configuration child class

--- config.py
+++ config.py
@@ -19,3 +19,19 @@ class ProdConfig(Config):
         Config: The parent configuration class with general configuration settings
     '''
     pass
+
+
+class DevConfig(Config):
+    '''
+    Development configuration child class
+
+    Args:
+        Config: The parent configuration class with general configuration settings
+    '''
+    DEBUG = True
+
+
+config_options = {
+    'development': DevConfig,
+    'production': ProdConfig
+}
