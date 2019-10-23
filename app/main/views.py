c5a4fcd94393eac4b9c21a8ba674d5b1cfc0adf8
kwayne <wkudoi@gmail.com>
2019-10-24 02:57:02 +0300

    created view function for the root page

---
+++ app/main/views.py
@@ -0,0 +1,21 @@
+from flask import render_template, request, redirect, url_for
+from . import main
+
+#views
+@main.route('/')
+def index():
+    '''
+    view root function that returns the index page and its data
+    '''
+
+    business_news = get_sources('business')
+    entertainment_news = get_sources('entertainment')
+    general_news = get_sources('general')
+    health_news = get_sources('health')
+    science_news = get_sources('science')
+    sports_news = get_sources('sports')
+    technology_news = get_sources('technology')
+
+    title = 'Welcome to Global News'
+
+    return render_template('index.html', title=title, business=business_news, entertainment=entertainment_news, general=general_news, health=health_news, science=science_news, sports=sports_news, technology=technology_news)
