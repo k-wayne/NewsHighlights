f0cfa73bb4209c776944c2d387db4df701f5abcf
kwayne <wkudoi@gmail.com>
2019-10-24 03:29:24 +0300

    define function to create object of results

--- app/requests.py
+++ app/requests.py
@@ -34,3 +34,31 @@ def get_sources(category):
             source_results = process_results(source_results_list)
 
     return source_results
+
+
+def process_results(source_list):
+    '''
+    Function that processes the source result and transforms them to a list of objects
+    
+    Args:
+        source_list: A list of dictionaries that contain source details
+
+    Returns:
+        source_results: A lost of source objects
+    '''
+
+    source_results = []
+    for source_item in source_list:
+        id = source_item.get("id")
+        name = source_item.get("name")
+        description = source_item.get("description")
+        url = source_item.get("url")
+        category = source_item.get("category")
+        language = source_item.get("language")
+        country = source_item.get("country")
+
+        source_object = Source(id, name, description, url,
+                               category, language, country)
+        source_results.append(source_object)
+
+    return source_results
