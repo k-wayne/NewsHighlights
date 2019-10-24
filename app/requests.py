e370149a7e8f0fecc1c236ff792aa979666e4e99
kwayne <wkudoi@gmail.com>
2019-10-24 03:27:22 +0300

    define function to fetch sources to our url

--- app/requests.py
+++ app/requests.py
@@ -15,3 +15,22 @@ def configure_request(app):
     api_key = app.config['NEWS_API_KEY']
     base_url = app.config['NEWS_API_BASE_URL']
     article_url = app.config['NEWS_ARTICLE_URL']
+
+
+def get_sources(category):
+    '''
+    Function that gets the json response to our url request
+    '''
+    get_sources_url = base_url.format(category, api_key)
+
+    with urllib.request.urlopen(get_sources_url) as url:
+        get_sources_data = url.read()
+        get_sources_response = json.loads(get_sources_data)
+
+        source_results = None
+
+        if get_sources_response['sources']:
+            source_results_list = get_sources_response['sources']
+            source_results = process_results(source_results_list)
+
+    return source_results
